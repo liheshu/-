935fbbfd8d1d11cb4f1f07169258afb53b01563f
xujiale <1192709841@qq.com>
2023-02-01 14:12:52 +0800

    提交文件

---
+++ src/components/hintButton/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <a :title="tip">
+          <el-button  v-bind="$attrs" v-on="$listeners"></el-button>
+      </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hintButton',
+  props:['tip'],
+  mounted(){
+    console.log(this.$attrs);
+    console.log(this.$listeners);
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
